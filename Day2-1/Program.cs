4e19d3e65595d734306f1b66d6a342b6d025aae7
Konstantin <lachner.k1@gmail.com>
2023-12-10 11:13:00 +0100

    add day 2

---
+++ Day2-1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+var sr = new StreamReader(@"..\..\..\Data\input.txt");
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+int redCubes = 12;
+int greenCubes = 13;
+int blueCubes = 14;
+
+bool validGame;
+
+int value = 0;
+
+string patternRed= "([0-9]+\\s+){1}red";
+string patternGreen = "([0-9]+\\s+){1}green";
+string patternBlue = "([0-9]+\\s+){1}blue";
+
+while (sr.Peek() >= 0)
+{
+    var line = sr.ReadLine();
+    string[] split = line.Split(':');
+    var sets = split[1].Split(";");
+
+    validGame = true;
+    foreach (var set in sets)
+    {
+        MatchCollection matchesRed = Regex.Matches(set, patternRed);
+        var countRed = int.Parse(matchesRed.FirstOrDefault()?.Groups[1].ToString() ?? "0");
+        if (countRed > redCubes)
+        {
+            validGame = false;
+            break;
+        }
+
+        MatchCollection matchesGreen = Regex.Matches(set, patternGreen);
+        var countGreen = int.Parse(matchesGreen.FirstOrDefault()?.Groups[1].ToString() ?? "0");
+        if (countGreen > greenCubes)
+        {
+            validGame = false;
+            break;
+        }
+
+        MatchCollection matchesBlue = Regex.Matches(set, patternBlue);
+        var countBlue = int.Parse(matchesBlue.FirstOrDefault()?.Groups[1].ToString() ?? "0");
+        if (countBlue > blueCubes)
+        {
+            validGame = false;
+            break;
+        }
+    }
+    if(validGame)
+    {
+        value += int.Parse(split[0].Split(' ')[1]);
+    }
+}
+
+Console.WriteLine(value);
+stopWatch.Stop();
+TimeSpan ts = stopWatch.Elapsed;
+Console.WriteLine("RunTime " + ts.ToString());
---
+++ Day2-2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+var sr = new StreamReader(@"..\..\..\Data\input.txt");
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+int value = 0;
+
+string patternRed = "([0-9]+\\s+){1}red";
+string patternGreen = "([0-9]+\\s+){1}green";
+string patternBlue = "([0-9]+\\s+){1}blue";
+
+while (sr.Peek() >= 0)
+{
+    var line = sr.ReadLine();
+    string[] split = line.Split(':');
+    var sets = split[1].Split(";");
+
+    int minimumRed = 0;
+    int minimumGreen = 0;
+    int minimumBlue = 0;
+
+    foreach (var set in sets)
+    {
+        MatchCollection matchesRed = Regex.Matches(set, patternRed);
+        var countRed = int.Parse(matchesRed.FirstOrDefault()?.Groups[1].ToString() ?? "0");
+        if (countRed > minimumRed)
+        {
+            minimumRed = countRed;
+        }
+
+        MatchCollection matchesGreen = Regex.Matches(set, patternGreen);
+        var countGreen = int.Parse(matchesGreen.FirstOrDefault()?.Groups[1].ToString() ?? "0");
+        if (countGreen > minimumGreen)
+        {
+            minimumGreen = countGreen;
+        }
+
+        MatchCollection matchesBlue = Regex.Matches(set, patternBlue);
+        var countBlue = int.Parse(matchesBlue.FirstOrDefault()?.Groups[1].ToString() ?? "0");
+        if (countBlue > minimumBlue)
+        {
+            minimumBlue = countBlue;
+        }
+    }
+
+    value += minimumRed * minimumGreen * minimumBlue;
+}
+
+Console.WriteLine(value);
+stopWatch.Stop();
+TimeSpan ts = stopWatch.Elapsed;
+Console.WriteLine("RunTime " + ts.ToString());
